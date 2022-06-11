6f88180cd4d57dd8c058af55c334e85b4b270b59
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-11 20:03:24 +0300

    addNumber()

--- solution/index.js
+++ solution/index.js
@@ -16,13 +16,46 @@ const result = document.querySelector(".result");
 const inputNumber = document.querySelector(".inputNumber");
 const btnContainer = document.querySelector(".buttons-container");
 
-let finalResult = 0;
-let operator = "";
-let activeOperator = false;
+let currentOperand = "";
+let previousOperand = "";
+let operation = "";
+let equalOrPercentPresssed = false;
 
-document.querySelector("#ac").addEventListener("click", () => {
-  document.querySelector(".result").innerHTML = "0";
+//? Event definition for the container carrying the buttons (.buttons-container):
+
+btnContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("number")) {
+    addNumber(e.target.textContent);
+    updateDisplay();
+  }
 });
+
+const addNumber = (num) => {
+  //? Return if 0 was entered before and 0 is entered again:
+  if (currentOperand === "0" && num === "0") return;
+
+  //? If 0 is entered first and then another number other than the . is entered, just pass the new number entered to the variable:
+  if (currentOperand === "0" && num !== ".") {
+    currentOperand = num;
+    return;
+  }
+
+  //? Return if current number is . and previous entered number contains .:
+  if (num === "." && currentOperand.includes(".")) return;
+
+  //? to prevent overflow:
+  if (currentOperand.length > 10) return;
+
+  //?The flag method is used to prevent overwriting when a new number is entered while there is a number on the screen:
+  if (equalOrPercentPresssed) {
+    currOperand = num;
+    equalOrPercentPresssed = false;
+    return;
+  }
+
+  //? Concatenate entered numbers:
+  currentOperand += num;
+};
 
 document.querySelector("#sign").addEventListener("click", () => {
   if (firstChar() === "-") {
